2dfbdc31d697b71262f0cece45b2f73d7d175ccc
Sabalvaro, Enrik P <enrik.p.sabalvaro@accenture.com>
2020-10-31 09:02:35 +0800

    fix email

--- routes/api/email.js
+++ routes/api/email.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
 
 router.post("/", async (req, res) => {
-  let transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "enriksabalvaro7@gmail.com",
-      pass: "enrik123",
-    },
-  });
+  let transport = nodemailer.createTransport(
+    smtpTransport({
+      service: "gmail",
+      secure: false,
+      auth: {
+        user: "enriksabalvaro7@gmail.com",
+        pass: "enrik123",
+      },
+    })
+  );
 
   const message = {
     from: req.body.email, // Sender address
